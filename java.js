386727e081301cc3a44bcff4848b90bea3f17ea6
Alejandro Lizarraga <lizarraga.alo55@gmail.com>
2023-06-14 10:56:47 -0700

    adding style to the page, creating a button for adding books and a container for the new books

--- java.js
+++ java.js
@@ -6,10 +6,22 @@ function Book (title, author, pages, read){
     this.pages = pages
     this.read = read
     this.info = function() {
-        return title, author, pages, read;
+        return title, author, pages, read; 
     }
 }
 
 function addBookToLibrary() {
-  // do stuff here
+  const book1 = new Book('titulo', 'autor', '150', 'not read it')
+  const book2 = new Book('titulo2', 'autor2', '200', 'read it')
+  myLibrary.push(book1, book2);
 }
+
+addBookToLibrary();
+
+function loopLibrary() {
+  myLibrary.forEach((book)=>{
+    console.log(book.info());
+  })
+}
+
+loopLibrary()
