29b4afee656f69b153651631bd118175b093526c
Alejandro Lizarraga <lizarraga.alo55@gmail.com>
2023-06-15 16:25:19 -0700

    creating the function that creates the card of the books on the html

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Project-library
 I will created a small online library.
+
+Credits for the creator of the photo:
+The inside of the library:
+Pexels.com Ivo Rainha
+Instagram ivoafr
--- java.js
+++ java.js
@@ -1,5 +1,6 @@
 let myLibrary = [];
 let addBook = document.querySelector('#add-btn');
+const container = document.querySelector('.container');
 
 
 // create a new book
@@ -9,15 +10,39 @@ function Book (title, author, pages, read){
     this.pages = pages
     this.read = read
 }
+
+//this function saves the book in the container of the html document
+function showBook (){ 
+  container.textContent = '';
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i]; 
+    let card = document.createElement('div');
+    card.classList.add('card');
+    container.appendChild(card);
+
+    for (let j = 0; j < Object.values(book).length; j++){
+      let infoBook = document.createElement('div');
+      infoBook.classList.add('books');
+      infoBook.textContent = `${Object.values(book)[j]}`
+      card.appendChild(infoBook);
+    }
+
+  }
+} 
+
 //this function is very specific
 function addBookToLibrary() {
   let title = document.querySelector('#title').value;
+  title = "Title: " + title;
   let author = document.querySelector('#author').value;
+  author = "Author: " + author;
   let pages = document.querySelector('#pages').value;
+  pages = "Pages: " + pages;
   let read = document.querySelector('#read').checked;
+  read = read ? "Read it" : "Not read it";
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
-  console.log(myLibrary)
+  showBook();
 }
 
 //make the form visible in the page
@@ -26,6 +51,7 @@ addBook.addEventListener('click', () => {
   bookForm.style.display = "block";
 })
 
+//submit the information of the new book 
 document.querySelector('form').addEventListener('submit', (event) => {
   event.preventDefault();
   addBookToLibrary();
