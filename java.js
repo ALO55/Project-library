8ad18b31acd51b235510f99f6b969d8cd13b58c1
Alejandro Lizarraga <lizarraga.alo55@gmail.com>
2023-06-14 17:01:56 -0700

    adding the function to make the form visible and submit the book when the user click on save book

--- java.js
+++ java.js
@@ -1,27 +1,33 @@
 let myLibrary = [];
+let addBook = document.querySelector('#add-btn');
 
+
+// create a new book
 function Book (title, author, pages, read){
     this.title = title
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function() {
-        return title, author, pages, read; 
-    }
 }
-
+//this function is very specific
 function addBookToLibrary() {
-  const book1 = new Book('titulo', 'autor', '150', 'not read it')
-  const book2 = new Book('titulo2', 'autor2', '200', 'read it')
-  myLibrary.push(book1, book2);
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+  let pages = document.querySelector('#pages').value;
+  let read = document.querySelector('#read').checked;
+  let newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  console.log(myLibrary)
 }
 
-addBookToLibrary();
+//make the form visible in the page
+addBook.addEventListener('click', () => {
+  let bookForm = document.querySelector('form');
+  bookForm.style.display = "block";
+})
 
-function loopLibrary() {
-  myLibrary.forEach((book)=>{
-    console.log(book.info());
-  })
-}
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  addBookToLibrary();
+})
 
-loopLibrary()
